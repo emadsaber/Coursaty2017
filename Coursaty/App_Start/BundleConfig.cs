e6b5efcf637901cea8e2087007a3d2aa67f60cf0
emadsaber <omda.sharkawy@gmail.com>
2017-06-23 18:53:05 +0200

    Merged changes from the Course->Index View. Removed Unused references from CoursesController Added some files to the solution from old version

--- Coursaty/App_Start/BundleConfig.cs
+++ Coursaty/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace Coursaty
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+			"~/Content/MyStyle.css",
                       "~/Content/site.css"));
         }
     }
